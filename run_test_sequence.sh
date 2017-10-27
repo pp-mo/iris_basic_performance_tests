e504f4a2d8711ecc07c85a5050cb087a27ae1299
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2017-10-27 13:41:44 +0100

    New tests, initial results : evaluate with+without dask set-option for synchronous scheduler.

--- run_test_sequence.sh
+++ run_test_sequence.sh
@@ -6,30 +6,48 @@
 #
 
 source activate dask_speedchecks
+RESULTS_REPO=$(pwd)
 IRIS_REPO=~/git/iris/iris_main
 
-mkdir -p results/latest
-rm -f results/latest/*
+mkdir -p results/daskopts_latest
+rm -f results/daskopts_latest/*
 
 cd $IRIS_REPO
-git checkout v_1x13x0
-. ../rebuild.sh
-cd -
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_1v13_x1.txt
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_1v13_x2.txt
+
+if [ "$(git branch | grep v2_latest_WITH_daskopts)" != "" ]; then
+  echo "BRANCH EXISTS : v2_latest_WITH_daskopts"
+else
+  echo "MAKING BRANCH : v2_latest_WITH_daskopts @ 648727710"
+  git branch v2_latest_WITH_daskopts 648727710
+fi
+if [ "$(git branch | grep v2_latest_WITHOUT_daskopts)" != "" ]; then
+  echo "BRANCH EXISTS : v2_latest_WITHOUT_daskopts @ d0abb15f4"
+else
+  echo "MAKING BRANCH : v2_latest_WITHOUT_daskopts"
+  git branch v2_latest_WITHOUT_daskopts d0abb15f4
+fi
 
 cd $IRIS_REPO
-git checkout v_2.0a0
+git checkout v2_latest_WITH_daskopts
 . ../rebuild.sh
-cd -
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_2v0a0_x1.txt
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_2v0a0_x2.txt
+cd $RESULTS_REPO
+python standard_performance_tests.py 2>&1 | tee results/daskopts_latest/performance_test_outputs_WITH_daskdefaults_x1.txt
+python standard_performance_tests.py 2>&1 | tee results/daskopts_latest/performance_test_outputs_WITH_daskdefaults_x2.txt
 
 cd $IRIS_REPO
-git checkout nc_simple_chunks
+git checkout v2_latest_WITHOUT_daskopts
 . ../rebuild.sh
-cd -
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_SMALLCHUNKS_x1.txt
-python standard_performance_tests.py 2>&1 | tee results/latest/performance_test_outputs_SMALLCHUNKS_x2.txt
+cd $RESULTS_REPO
+python standard_performance_tests.py 2>&1 | tee results/daskopts_latest/performance_test_outputs_WITHOUT_daskdefaults_x1.txt
+python standard_performance_tests.py 2>&1 | tee results/daskopts_latest/performance_test_outputs_WITHOUT_daskdefaults_x2.txt
 
-
+cd $IRIS_REPO
+git checkout latest
+if [ "$(git branch | grep v2_latest_WITH_daskopts)" != "" ]; then
+  echo "DELETING BRANCH : v2_latest_WITH_daskopts"
+  git branch -D v2_latest_WITH_daskopts
+fi
+if [ "$(git branch | grep v2_latest_WITHOUT_daskopts)" != "" ]; then
+  echo "DELETING BRANCH : v2_latest_WITHOUT_daskopts"
+  git branch -D v2_latest_WITHOUT_daskopts
+fi
--- standard_performance_tests.py
+++ standard_performance_tests.py
@@ -64,6 +64,13 @@ def do_save(cubes, filename):
     return timer.seconds, None
 
 
+def do_mean(cube, coords):
+    with TimedOp() as timer:
+        result = cube.copy().collapsed(coords, iris.analysis.MEAN)
+        result.data
+    return timer.seconds, result
+
+
 _DEBUG = True
 
 _DEFAULT_REPS = 3
@@ -72,9 +79,12 @@ _DEFAULT_REPS = 3
 
 REPORT_LINES = []
 
-def exercise(description, method, args, repeats=None, abstol=0.001, reltol=0.08):
-    if repeats is None:
-        repeats = _DEFAULT_REPS
+def exercise(description, method, args,
+             n_measure_times=None, n_ops_per_measure=1,
+             abstol=0.001, reltol=0.08):
+
+    if n_measure_times is None:
+        n_measure_times = _DEFAULT_REPS
 
     times = []
     n_rep = 1
@@ -84,18 +94,25 @@ def exercise(description, method, args, repeats=None, abstol=0.001, reltol=0.08)
         print('  Running {!r} :'.format(description))
         sys.stdout.flush()
 
-    while n_rep <= repeats:
+    while n_rep <= n_measure_times:
         if _DEBUG:
             print('    repeat #{} .. '.format(n_rep), end='')
             sys.stdout.flush()
-        time, result = method(*args)
+        time = 0.0
+        for i_op in range(n_ops_per_measure):
+            time_i, result = method(*args)
+            time += time_i
+            if _DEBUG and n_ops_per_measure > 1:
+                print('    time #{} : {:12.6f} ...'.format(i_op, time_i))
+                sys.stdout.flush()
+        time /= n_ops_per_measure
         if _DEBUG:
-            print('took {:12.6f} ...'.format(time))
+            print('  TOOK {:12.6f} ...'.format(time))
             sys.stdout.flush()
         times.append(time)
         n_rep += 1
 
-    if repeats > 2:
+    if n_measure_times > 2:
         np.testing.assert_allclose(times[-2], times[-1],
                                    atol=abstol, rtol=reltol)
 
@@ -109,10 +126,7 @@ def exercise(description, method, args, repeats=None, abstol=0.001, reltol=0.08)
 
 
 def set_futures():
-    names = ('cell_datetime_objects',
-             'netcdf_promote',
-             'strict_grib_load',
-             'netcdf_no_unlimited')
+    names = ['netcdf_no_unlimited']
     for name in names:
         if hasattr(iris.FUTURE, name):
             setattr(iris.FUTURE, name, True)
@@ -125,13 +139,17 @@ def run_tests():
         '/data/local/dataZoo/PP/mogrepsR/2011032906.qtey06.oper00.000*.pp'
 
     pp_cubes = exercise('PP load (plain)',
-                        do_load, [pp_filespec])
+                        do_load, [pp_filespec],
+                        n_ops_per_measure=3)
 
     exercise('PP load (structured)',
-             do_structured_load, [pp_filespec])
+             do_structured_load, [pp_filespec],
+             n_ops_per_measure=4)
 
     exercise('PP data load',
-             do_data_load, [pp_cubes], repeats=4, reltol=0.07)
+             do_data_load, [pp_cubes],
+             n_measure_times=4, n_ops_per_measure=4,
+             reltol=0.07)
 
     ff_filespec = '/data/local/dataZoo/FF/ff_variants/ff_64.ff'
 
@@ -142,18 +160,32 @@ def run_tests():
              do_structured_load, [ff_filespec])
 
     exercise('Fieldsfile data load',
-             do_data_load, [ff_cubes[29:30]], repeats=4, reltol=0.07)
+             do_data_load, [ff_cubes[29:30]],
+             n_measure_times=4, n_ops_per_measure=4,
+             reltol=0.33)
+
+    exercise('Fieldsfile mean calc',
+             do_mean, [ff_cubes[29], ('time', 'pressure')],
+             n_ops_per_measure=5)
 
     nc_filespec = (
         '/data/local/dataZoo/netCDF/largeFile/'
         'tas_day_HadGEM2-ES_historical_r1i1p1_18591201-20051130.nc')
 
     nc_cubes = exercise('NetCDF load (plain)',
-                        do_load, [nc_filespec], repeats=4, reltol=0.15)
+                        do_load, [nc_filespec],
+                        n_measure_times=4, n_ops_per_measure=4,
+                        reltol=0.15)
 
     nc_dataload_cubes_list = [nc_cubes[0][:5000]]
     exercise('NetCDF data load',
-             do_data_load, [nc_dataload_cubes_list], repeats=4, reltol=0.4)
+             do_data_load, [nc_dataload_cubes_list],
+             n_measure_times=4,
+             reltol=0.33)
+
+    exercise('NetCDF mean calc',
+             do_mean, [nc_cubes[0][:5000], 'time'],
+             n_ops_per_measure=5)
 
     nc_save_cube = nc_cubes[0][:5000]
     exercise('PP save to PP',
